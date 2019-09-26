3143fb980739f12acd8af1a76dabb3edaa70ce6e
Tim Paine <t.paine154@gmail.com>
2019-09-26 00:07:49 -0400

    grab pgcontents code, issue with toggle

--- multicontentsmanager/metacontentsmanager.py
+++ multicontentsmanager/metacontentsmanager.py
@@ -1,5 +1,185 @@
+from datetime import datetime
+from tornado.web import HTTPError
 from notebook.services.contents.manager import ContentsManager
 from notebook.services.contents.largefilemanager import LargeFileManager
+
+# A reference implementation
+# https://github.com/quantopian/pgcontents/blob/master/pgcontents/hybridmanager.py
+# Apache 2.0
+# https://github.com/quantopian/pgcontents/blob/2ddca481532a4e983b4370dae8ca7f11da5e5c30/LICENSE
+
+DUMMY_CREATED_DATE = datetime.fromtimestamp(0)
+
+
+def base_model(path):
+    return {
+        "name": path.rsplit('/', 1)[-1],
+        "path": path,
+        "writable": True,
+        "last_modified": None,
+        "created": None,
+        "content": None,
+        "format": None,
+        "mimetype": None,
+    }
+
+
+def base_directory_model(path):
+    m = base_model(path)
+    m.update(
+        type='directory',
+        last_modified=DUMMY_CREATED_DATE,
+        created=DUMMY_CREATED_DATE,
+    )
+    return m
+
+
+def _resolve_path(path, manager_dict):
+    """
+    Resolve a path based on a dictionary of manager prefixes.
+
+    Returns a triple of (prefix, manager, manager_relative_path).
+    """
+    parts = path.strip('/').split('/')
+
+    # Try to find a sub-manager for the first subdirectory.
+    mgr = manager_dict.get(parts[0].split(':')[0])
+    if mgr is not None:
+        return parts[0], mgr, '/'.join(parts[1:])
+
+    # Try to find use the root manager, if one was supplied.
+    mgr = manager_dict.get('')
+    if mgr is not None:
+        return '', mgr, path
+
+    raise HTTPError(
+        404,
+        "Couldn't resolve path [{path}] and "
+        "no root manager supplied!".format(path=path)
+    )
+
+
+def _get_arg(argname, args, kwargs):
+    """
+    Get an argument, either from kwargs or from the first entry in args.
+    Raises a TypeError if argname not in kwargs and len(args) == 0.
+
+    Mutates kwargs in place if the value is found in kwargs.
+    """
+    try:
+        return kwargs.pop(argname), args
+    except KeyError:
+        pass
+    try:
+        return args[0], args[1:]
+    except IndexError:
+        raise TypeError("No value passed for %s" % argname)
+
+
+def _apply_prefix(prefix, model):
+    """
+    Prefix all path entries in model with the given prefix.
+    """
+    if not isinstance(model, dict):
+        raise TypeError("Expected dict for model, got %s" % type(model))
+
+    # We get unwanted leading/trailing slashes if prefix or model['path'] are
+    # '', both of which are legal values.
+    model['path'] = '/'.join((prefix, model['path'])).strip('/')
+    if model['type'] in ('notebook', 'file'):
+        return model
+
+    if model['type'] != 'directory':
+        raise ValueError("Unknown model type %s." % type(model))
+
+    content = model.get('content', None)
+    if content is not None:
+        for sub_model in content:
+            _apply_prefix(prefix, sub_model)
+
+    return model
+
+
+# Dispatch decorators.
+def path_dispatch1(mname, returns_model):
+    """
+    Decorator for methods that accept path as a first argument.
+    """
+    def _wrapper(self, *args, **kwargs):
+        path, args = _get_arg('path', args, kwargs)
+        prefix, mgr, mgr_path = _resolve_path(path, self._contents_managers)
+        result = getattr(mgr, mname)(mgr_path, *args, **kwargs)
+        if returns_model and prefix:
+            return _apply_prefix(prefix, result)
+        else:
+            return result
+
+    return _wrapper
+
+
+def path_dispatch2(mname, first_argname, returns_model):
+    """
+    Decorator for methods that accept path as a second argument.
+    """
+    def _wrapper(self, *args, **kwargs):
+        other, args = _get_arg(first_argname, args, kwargs)
+        path, args = _get_arg('path', args, kwargs)
+        prefix, mgr, mgr_path = _resolve_path(path, self._contents_managers)
+        result = getattr(mgr, mname)(other, mgr_path, *args, **kwargs)
+        if returns_model and prefix:
+            return _apply_prefix(prefix, result)
+        else:
+            return result
+    return _wrapper
+
+
+def path_dispatch_kwarg(mname, path_default, returns_model):
+    """
+    Parameterized decorator for methods that accept path as a second
+    argument.
+    """
+    def _wrapper(self, path=path_default, **kwargs):
+        prefix, mgr, mgr_path = _resolve_path(path, self._contents_managers)
+        result = getattr(mgr, mname)(path=mgr_path, **kwargs)
+        if returns_model and prefix:
+            return _apply_prefix(prefix, result)
+        else:
+            return result
+    return _wrapper
+
+
+def path_dispatch_old_new(mname, returns_model):
+    """
+    Decorator for methods accepting old_path and new_path.
+    """
+    def _wrapper(self, old_path, new_path, *args, **kwargs):
+        old_prefix, old_mgr, old_mgr_path = _resolve_path(
+            old_path, self._contents_managers
+        )
+        new_prefix, new_mgr, new_mgr_path = _resolve_path(
+            new_path, self._contents_managers,
+        )
+        if old_mgr is not new_mgr:
+            # TODO: Consider supporting this via get+delete+save.
+            raise HTTPError(
+                400,
+                "Can't move files between backends ({old} -> {new})".format(
+                    old=old_path,
+                    new=new_path,
+                )
+            )
+        assert new_prefix == old_prefix
+        result = getattr(new_mgr, mname)(
+            old_mgr_path,
+            new_mgr_path,
+            *args,
+            **kwargs
+        )
+        if returns_model and new_prefix:
+            return _apply_prefix(new_prefix, result)
+        else:
+            return result
+    return _wrapper
 
 
 class MetaContentsManager(ContentsManager):
@@ -14,294 +194,79 @@ class MetaContentsManager(ContentsManager):
         self._inited = True
         self._contents_managers.update({_[0]: _[1](**self._kwargs) if isinstance(_[1], type) else _[1] for _ in (managers or {}).items()})
 
-    def _which_manager(self, path):
-        for k in self._contents_managers:
-            if not k:
-                continue
-            basepath = k + ':'
-            if path.startswith(basepath):
-                return self._contents_managers[k], path.replace(basepath, '', 1)
-            elif path.startswith('/') and path[1:].startswith(basepath):
-                return self._contents_managers[k], path.replace(basepath, '', 1)
-        return self._contents_managers[''], path
+    @property
+    def root_manager(self):
+        return self._contents_managers.get('')
 
-    def run_pre_save_hook(self, model, path, **kwargs):
-        """Run the pre-save hook if defined, and log errors"""
-        manager, path = self._which_manager(path)
-        return manager.run_pre_save_hook(model, path)
+    def _extra_root_dirs(self):
+        return [
+            base_directory_model(path)
+            for path in self._contents_managers
+            if path
+        ]
 
-    def run_post_save_hook(self, model, path, **kwargs):
-        """Run the post-save hook if defined, and log errors"""
-        manager, path = self._which_manager(path)
-        return manager.run_post_save_hook(model, path)
+    is_hidden = path_dispatch1('is_hidden', False)
+    dir_exists = path_dispatch1('dir_exists', False)
+    file_exists = path_dispatch_kwarg('file_exists', '', False)
+    exists = path_dispatch1('exists', False)
 
-    def get_extra_handlers(self):
-        """Return additional handlers
+    save = path_dispatch2('save', 'model', True)
+    rename = path_dispatch_old_new('rename', False)
 
-        Default: self.files_handler_class on /files/.*
-        """
-        handlers = []
-        for manager in self._contents_managers.values():
-            handlers.extend(manager.get_extra_handlers())
-        return handlers
-
-    def dir_exists(self, path):
-        """Does a directory exist at the given path?
-
-        Like os.path.isdir
-
-        Override this method in subclasses.
-
-        Parameters
-        ----------
-        path : string
-            The path to check
-
-        Returns
-        -------
-        exists : bool
-            Whether the path does indeed exist.
-        """
-        manager, path = self._which_manager(path)
-        return manager.dir_exists(path)
-
-    def is_hidden(self, path):
-        """Is path a hidden directory or file?
-
-        Parameters
-        ----------
-        path : string
-            The path to check. This is an API path (`/` separated,
-            relative to root dir).
-
-        Returns
-        -------
-        hidden : bool
-            Whether the path is hidden.
-
-        """
-        manager, path = self._which_manager(path)
-        return manager.is_hidden(path)
-
-    def file_exists(self, path=''):
-        """Does a file exist at the given path?
-
-        Like os.path.isfile
-
-        Override this method in subclasses.
-
-        Parameters
-        ----------
-        path : string
-            The API path of a file to check for.
-
-        Returns
-        -------
-        exists : bool
-            Whether the file exists.
-        """
-        manager, path = self._which_manager(path)
-        return manager.file_exists(path)
-
-    def exists(self, path):
-        """Does a file or directory exist at the given path?
-
-        Like os.path.exists
-
-        Parameters
-        ----------
-        path : string
-            The API path of a file or directory to check for.
-
-        Returns
-        -------
-        exists : bool
-            Whether the target exists.
-        """
-        manager, path = self._which_manager(path)
-        return manager.exists(path)
+    __get = path_dispatch1('get', True)
+    __delete = path_dispatch1('delete', False)
 
     def get(self, path, content=True, type=None, format=None):
-        """Get a file or directory model."""
-        manager, path = self._which_manager(path)
-        return manager.get(path, content, type, format)
-
-    def save(self, model, path):
         """
-        Save a file or directory model to path.
-
-        Should return the saved model with no content.  Save implementations
-        should call self.run_pre_save_hook(model=model, path=path) prior to
-        writing any data.
+        Special case handling for listing root dir.
         """
-        manager, path = self._which_manager(path)
-        return manager.save(model, path)
+        path = path.strip('/')
+        if path:
+            return self.__get(path, content=content, type=type, format=format)
+        if not content:
+            return base_directory_model('')
 
-    def delete_file(self, path):
-        """Delete the file or directory at path."""
-        manager, path = self._which_manager(path)
-        return manager.delete_file(path)
-
-    def rename_file(self, old_path, new_path):
-        """Rename a file or directory."""
-        manager, old_path = self._which_manager(old_path)
-        _, new_path = self._which_manager(new_path)
-        return manager.rename_file(old_path, new_path)
+        extra_content = self._extra_root_dirs()
+        rm = self.root_manager
+        if rm is None:
+            root_model = base_directory_model('')
+            root_model.update(
+                format='json',
+                content=extra_content,
+            )
+        else:
+            root_model = rm.get(
+                path,
+                content=content,
+                type=type,
+                format=format,
+            )
+            # Append the extra directories.
+            root_model['content'].extend(extra_content)
+        return root_model
 
     def delete(self, path):
-        """Delete a file/directory and any associated checkpoints."""
-        manager, path = self._which_manager(path)
-        return manager.delete(path)
-
-    def rename(self, old_path, new_path):
-        """Rename a file and any checkpoints associated with that file."""
-        manager, old_path = self._which_manager(old_path)
-        _, new_path = self._which_manager(new_path)
-        print(old_path, new_path)
-        return manager.rename(old_path, new_path)
-
-    def update(self, model, path):
-        """Update the file's path
-
-        For use in PATCH requests, to enable renaming a file without
-        re-uploading its contents. Only used for renaming at the moment.
         """
-        manager, path = self._which_manager(path)
-        return manager.update(model, path)
-
-    def info_string(self):
-        return "Serving contents"
-
-    def get_kernel_path(self, path, model=None):
-        """Return the API path for the kernel
-
-        KernelManagers can turn this value into a filesystem path,
-        or ignore it altogether.
-
-        The default value here will start kernels in the directory of the
-        notebook server. FileContentsManager overrides this to use the
-        directory containing the notebook.
+        Ensure that roots of our managers can't be deleted.  This should be
+        enforced by https://github.com/ipython/ipython/pull/8168, but rogue
+        implementations might override this behavior.
         """
-        manager, path = self._which_manager(path)
-        return manager.get_kernel_path(path, model)
+        path = path.strip('/')
+        if path in self._contents_managers:
+            raise HTTPError(
+                400, "Can't delete root of %s" % self._contents_managers[path]
+            )
+        return self.__delete(path)
 
-    def increment_filename(self, filename, path='', insert=''):
-        """Increment a filename until it is unique.
-
-        Parameters
-        ----------
-        filename : unicode
-            The name of a file, including extension
-        path : unicode
-            The API path of the target's directory
-        insert: unicode
-            The characters to insert after the base filename
-
-        Returns
-        -------
-        name : unicode
-            A filename that is unique, based on the input filename.
-        """
-        manager, path = self._which_manager(path)
-        return manager.increment_filename(filename, path, insert)
-
-    def new_untitled(self, path='', type='', ext=''):
-        """Create a new untitled file or directory in path
-
-        path must be a directory
-
-        File extension can be specified.
-
-        Use `new` to create files with a fully specified path (including filename).
-        """
-        manager, path = self._which_manager(path)
-        return manager.new_untitled(path, type, ext)
-
-    def new(self, model=None, path=''):
-        """Create a new file or directory and return its model with no content.
-
-        To create a new untitled entity in a directory, use `new_untitled`.
-        """
-        manager, path = self._which_manager(path)
-        return manager.new(model, path)
-
-    def copy(self, from_path, to_path=None):
-        """Copy an existing file and return its new model.
-
-        If to_path not specified, it will be the parent directory of from_path.
-        If to_path is a directory, filename will increment `from_path-Copy#.ext`.
-        Considering multi-part extensions, the Copy# part will be placed before the first dot for all the extensions except `ipynb`.
-        For easier manual searching in case of notebooks, the Copy# part will be placed before the last dot.
-
-        from_path must be a full path to a file.
-        """
-        manager, from_path = self._which_manager(from_path)
-        _, to_path = self._which_manager(to_path)
-        return manager.copy(from_path, to_path)
-
-    def trust_notebook(self, path):
-        """Explicitly trust a notebook
-
-        Parameters
-        ----------
-        path : string
-            The path of a notebook
-        """
-        manager, path = self._which_manager(path)
-        return manager.trust_notebook(path)
-
-    def check_and_sign(self, nb, path=''):
-        """Check for trusted cells, and sign the notebook.
-
-        Called as a part of saving notebooks.
-
-        Parameters
-        ----------
-        nb : dict
-            The notebook dict
-        path : string
-            The notebook's path (for logging)
-        """
-        manager, path = self._which_manager(path)
-        return manager.check_and_sign(nb, path)
-
-    def mark_trusted_cells(self, nb, path=''):
-        """Mark cells as trusted if the notebook signature matches.
-
-        Called as a part of loading notebooks.
-
-        Parameters
-        ----------
-        nb : dict
-            The notebook object (in current nbformat)
-        path : string
-            The notebook's path (for logging)
-        """
-        manager, path = self._which_manager(path)
-        return manager.mark_trusted_cells(nb, path)
-
-    def should_list(self, name):
-        """Should this file/directory name be displayed in a listing?"""
-        manager, name = self._which_manager(name)
-        return manager.should_list(name)
-
-    # Part 3: Checkpoints API
-    def create_checkpoint(self, path):
-        """Create a checkpoint."""
-        manager, path = self._which_manager(path)
-        return manager.create_checkpoint(path)
-
-    def restore_checkpoint(self, checkpoint_id, path):
-        """
-        Restore a checkpoint.
-        """
-        manager, path = self._which_manager(path)
-        return manager.restore_checkpoint(checkpoint_id, path)
-
-    def list_checkpoints(self, path):
-        manager, path = self._which_manager(path)
-        return manager.list_checkpoints(path)
-
-    def delete_checkpoint(self, checkpoint_id, path):
-        manager, path = self._which_manager(path)
-        return manager.delete_checkpoint(checkpoint_id, path)
+    create_checkpoint = path_dispatch1('create_checkpoint', False)
+    list_checkpoints = path_dispatch1('list_checkpoints', False)
+    restore_checkpoint = path_dispatch2(
+        'restore_checkpoint',
+        'checkpoint_id',
+        False,
+    )
+    delete_checkpoint = path_dispatch2(
+        'delete_checkpoint',
+        'checkpoint_id',
+        False,
+    )
