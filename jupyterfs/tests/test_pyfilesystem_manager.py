b43a5232473e6a52212a76695ca8cb81f0d68730
telamonian <mklein@jhu.edu>
2020-03-20 02:04:21 -0400

    linted test_pyfilesystem_manager.py

--- jupyterfs/tests/test_pyfilesystem_manager.py
+++ jupyterfs/tests/test_pyfilesystem_manager.py
@@ -14,33 +14,40 @@ test_contents = 'foo/nbar/nbaz'
 test_endpoint_url = 'http://127.0.0.1:9000'
 test_fname = 'foo.txt'
 
-def _s3client():
+
+def _s3Client():
     return boto3.client(
-    's3',
-    endpoint_url=test_endpoint_url,
-    # aws_access_key_id=test_cred,
-    # aws_secret_access_key=test_cred,
-    # region_name=self.region,
-)
+        's3',
+        endpoint_url=test_endpoint_url,
+        # aws_access_key_id=test_cred,
+        # aws_secret_access_key=test_cred,
+        # region_name=self.region,
+    )
+
+
+def _s3ContentsManager():
+    s3Uri = 's3://{bucket}?endpoint_url={endpoint_url}'.format(bucket=test_bucket, endpoint_url=test_endpoint_url)
+    return PyFilesystemContentsManager.open_fs(s3Uri)
+
 
 class TestPyFilesystemContentsManagerS3:
     def setup_method(self, method):
-        s3 = _s3client()
+        s3client = _s3Client()
 
         # create a test bucket
-        s3.create_bucket(Bucket=test_bucket)
+        s3client.create_bucket(Bucket=test_bucket)
 
     def teardown_method(self, method):
-        s3 = _s3client()
+        s3client = _s3Client()
 
         # delete all buckets
-        for bucket in s3.list_buckets():
+        for bucket in s3client.list_buckets():
             for key in bucket.objects.all():
                 key.delete()
             bucket.delete()
 
     def test_write_s3_read_s3(self):
-        s3man = PyFilesystemContentsManager.open_fs('s3://{bucket}?endpoint_url={endpoint_url}'.format(bucket=test_bucket, endpoint_url=test_endpoint_url))
+        s3CM = _s3ContentsManager()
 
         fpaths = [
             '' + test_fname,
@@ -49,19 +56,19 @@ class TestPyFilesystemContentsManagerS3:
         ]
 
         # set up dir structure
-        s3man._save_directory('root0', None)
-        s3man._save_directory('root1', None)
-        s3man._save_directory('root1/leaf1', None)
+        s3CM._save_directory('root0', None)
+        s3CM._save_directory('root1', None)
+        s3CM._save_directory('root1/leaf1', None)
 
         # save to root and tips
-        s3man.save(test_contents, fpaths[0])
-        s3man.save(test_contents, fpaths[1])
-        s3man.save(test_contents, fpaths[2])
+        s3CM.save(test_contents, fpaths[0])
+        s3CM.save(test_contents, fpaths[1])
+        s3CM.save(test_contents, fpaths[2])
 
         # read and check
-        assert test_contents == s3man.get(fpath[0])
-        assert test_contents == s3man.get(fpath[1])
-        assert test_contents == s3man.get(fpath[2])
+        assert test_contents == s3CM.get(fpaths[0])
+        assert test_contents == s3CM.get(fpaths[1])
+        assert test_contents == s3CM.get(fpaths[2])
 
     # @classmethod
     # def setup_class(cls):
