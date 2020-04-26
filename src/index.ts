360dd916c4dfeb167d96b427b815cb43d9a49159
telamonian <telamonian@users.noreply.github.com>
2020-04-26 19:20:33 -0400

    refactored current FileTree init into ftFromServerextension

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,9 @@ import { IWindowResolver } from "@jupyterlab/apputils";
 import { PageConfig } from "@jupyterlab/coreutils";
 import { IDocumentManager } from "@jupyterlab/docmanager";
 import { ISettingRegistry } from "@jupyterlab/settingregistry";
+import { DisposableSet } from "@lumino/disposable";
 
-import {constructFileTreeWidget} from "./filetree";
+import { constructFileTreeWidget } from "./filetree";
 
 // tslint:disable: variable-name
 
@@ -40,33 +41,44 @@ async function activate(
   settingRegistry: ISettingRegistry,
 ) {
   // Attempt to load application settings
-  // let settings: ISettingRegistry.ISettings;
+  let settings: ISettingRegistry.ISettings;
   try {
-    // settings = await settingRegistry.load(plugin.id);
-    await settingRegistry.load(plugin.id);
+    settings = await settingRegistry.load(plugin.id);
   } catch (error) {
     // tslint:disable-next-line:no-console
     console.warn(`Failed to load settings for the jupyter-fs extension.\n${error}`);
   }
 
   // grab templates from serverextension
-  fetch(new Request(PageConfig.getBaseUrl() + "multicontents/get",
-                    {method: "get"})).then(async (value: Response) => {
-    if (value.ok) {
-      const keys = await value.json() as string[];
+  function ftFromServerextension(disposable: DisposableSet) {
+    fetch(new Request(PageConfig.getBaseUrl() + "multicontents/get", {method: "get"})).then(async (val) => {
+      if (val.ok) {
+        const keys = await val.json() as string[];
 
-      // tslint:disable-next-line:no-console
-      console.log("JupyterLab extension jupyter-fs is activated!");
-      for ( const s of keys) {
-        constructFileTreeWidget(app, s, s, "left", paths, resolver, restorer, manager, router);
         // tslint:disable-next-line:no-console
-        console.log("Adding contents manager for " + s);
+        console.log("JupyterLab extension jupyter-fs is activated!");
+        for ( const s of keys) {
+          disposable.add(constructFileTreeWidget(app, s, s, "left", paths, resolver, restorer, manager, router));
+          // tslint:disable-next-line:no-console
+          console.log("Adding contents manager for " + s);
+        }
+      } else {
+        // tslint:disable-next-line:no-console
+        console.warn("jupyter-fs failed to activate");
       }
-    } else {
-      // tslint:disable-next-line:no-console
-      console.warn("jupyter-fs failed to activate");
+    });
+  }
+
+  const _d = new DisposableSet();
+  if (settings) {
+    function onSettingsChange(settings: ISettingRegistry.ISettings) {
+      _d.dispose();
+
+      ftFromServerextension(_d);
     }
-  });
+
+    settings.changed.connect(onSettingsChange, this);
+  }
 }
 
 export default plugin;
