9918fdd0539d0c51126c94e817b4167e80de6a16
Reo Ono <1992reo@gmail.com>
2022-04-01 23:54:23 +0900

    fix job to install docker for mac

--- ci/start_docker_desktop_mac.sh
+++ ci/start_docker_desktop_mac.sh
@@ -1,40 +1,9 @@
 #!/usr/bin/env bash
 
 # refs:
-# https://stackoverflow.com/a/35979292
-# https://crossprogramming.com/2019/12/27/use-docker-when-running-integration-tests-with-azure-pipelines.html#self-managed-docker-containers
+# https://github.com/docker/for-mac/issues/2359#issuecomment-943131345
 
-[[ $(uname) == 'Darwin' ]] || { echo "This function only runs on macOS." >&2; exit 2; }
-
-# show if Docker.app is present
-set -x
-ls /Applications
-set +x
-
-printf "\nStarting Docker.app, if necessary"
-
-# Wait for the server to start up, if applicable.
-retries=0
-while ! docker system info &>/dev/null; do
-    if (( retries % 30 == 0 )); then
-        if pgrep -xq -- "Docker"; then
-            printf '\nDocker init still running'
-        else
-            (( retries != 0 )) && printf '\nDocker not running, restart'
-            # /Applications/Docker.app/Contents/MacOS/Docker &
-            open -g -a Docker.app || exit
-        fi
-
-        if [[ ${retries} -gt 600 ]]; then
-            printf '\nFailed to run Docker'
-            exit 1
-        fi
-    fi
-
-    printf '.'
-    (( retries++))
-    sleep 1
-done
-(( retries )) && printf '\n'
+open -a /Applications/Docker.app --args --unattended --accept-license
+while ! /Applications/Docker.app/Contents/Resources/bin/docker info &>/dev/null; do sleep 1; done
 
 echo "Docker is ready"
