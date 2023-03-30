23673f0e1924870c8228c0acf338740d307696ae
Dave Malvin Limanda <47667667+davemalvin@users.noreply.github.com>
2023-03-30 10:56:42 +0100

    Change default to only size

--- js/src/treefinder.ts
+++ js/src/treefinder.ts
@@ -217,7 +217,6 @@ export class TreeFinderWidget extends Widget {
   }
   
   cm: JupyterContents;
-  commands: any;
   rootPath: string;
   columns: (keyof JupyterContents.IJupyterContentRow)[];
   settings: ISettingRegistry.ISettings;
